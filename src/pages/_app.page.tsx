5b0c032dedea9f38e469a7e6fadcdf90c93433d0
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-03 19:47:32 +0900

    🩹 chore

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,16 +1,8 @@
 import "src/styles/global.css";
 
 import type { AppPropsWithLayout } from "next/app";
-import { usePageView } from "src/lib/gtag";
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
-  // if (process.env.NODE_ENV === 'development') {
-  //   const MockServer = () => import('mocks//msw/worker');
-  //   MockServer();
-  // }
-
-  usePageView();
-
   const getLayout =
     Component.getLayout ??
     ((page) => {
