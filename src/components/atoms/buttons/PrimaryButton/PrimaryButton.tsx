3c02a646a63d725c20a9a2b1257b77e08cd35cca
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-03 17:08:17 +0900

    🔥 remove

--- src/components/atoms/buttons/PrimaryButton/PrimaryButton.tsx
+++
@@ -1,7 +0,0 @@
-export type Props = JSX.IntrinsicElements["button"] & {
-  children: React.ReactNode;
-};
-
-export const PrimaryButton: React.FC<Props> = ({ children, ...restProps }) => {
-  return <button {...restProps}>{children}</button>;
-};
