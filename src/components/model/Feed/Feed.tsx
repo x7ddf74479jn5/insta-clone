619abfda44d4758e77d977ed3a96a4513d82f859
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-08 00:35:38 +0900

    ✨ feat: Suggestion

--- src/components/model/Feed/Feed.tsx
+++ src/components/model/Feed/Feed.tsx
@@ -1,9 +1,11 @@
+import { MiniProfile } from "src/components/model/MiniProfile";
 import { Posts } from "src/components/model/Post";
 import { Stories } from "src/components/model/Story";
+import { Suggestions } from "src/components/model/Suggestion";
 
 export const Feed = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl">
+    <div className="grid grid-cols-1 mx-auto md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl">
       {/* Section */}
       <section className="col-span-2">
         <Stories />
@@ -11,9 +13,11 @@ export const Feed = () => {
       </section>
 
       {/* Section */}
-      <section>
-        {/* Mini Profile */}
-        {/* Suggestions */}
+      <section className="hidden md:col-span-1 xl:inline-grid">
+        <div className="fixed top-20">
+          <MiniProfile />
+          <Suggestions />
+        </div>
       </section>
     </div>
   );
---
+++ src/components/model/Suggestion/Suggestions.tsx
@@ -0,0 +1,24 @@
+import { useSuggestions } from "../../hooks/useSuggestions";
+
+export const Suggestions = () => {
+  const [suggestions] = useSuggestions(5);
+  return (
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between mb-5 text-sm">
+        <h3 className="text-sm font-bold text-gray-400">Suggestions for you </h3>
+        <button className="font-semibold text-gray-600">See All</button>
+      </div>
+
+      {suggestions.map((profile) => (
+        <div key={profile.id} className="flex justify-between items-center mt-3">
+          <img src={profile.avatar} alt="" className="p-[2px] w-10 h-10 rounded-full border" />
+          <div className="flex-1 ml-4">
+            <h2 className="text-sm font-semibold">{profile.username}</h2>
+            <h3 className="text-xs text-gray-400">Works at {profile.company.name}</h3>
+          </div>
+          <button className="text-sm text-blue-400">Follow</button>
+        </div>
+      ))}
+    </div>
+  );
+};
