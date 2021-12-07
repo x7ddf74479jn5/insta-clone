ab799a8b7b9c9d94eb09b0d91312ad2074864e81
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-07 21:17:11 +0900

    🏷️ add: types

---
+++ src/types/profile.ts
@@ -0,0 +1,12 @@
+export type TProfile = {
+  id: number;
+  name: string;
+  username: string;
+  avatar: string;
+  email: string;
+  dob: Date;
+  phone: string;
+  address: Faker.Address;
+  website: string;
+  company: Faker.Company;
+};
