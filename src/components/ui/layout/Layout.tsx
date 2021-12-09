fe4c11ab48c18b3efa432d07bedbf1bac3060385
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-10 02:06:34 +0900

    ⏪ revert: layout

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout: CustomLayout = (page) => {
   return (
     <div className="overflow-y-scroll h-screen bg-gray-50 scrollbar-hide">
       <Header />
-      <main className="grid grid-cols-1 mx-auto md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl">{page}</main>
+      <main>{page}</main>
       <Footer />
     </div>
   );
