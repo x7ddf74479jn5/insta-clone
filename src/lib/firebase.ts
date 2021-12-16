9d88c363996b2d348f7ebdc3a4b82b8870ff63fb
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-17 02:47:17 +0900

    ✨ feat: type-safe firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import type { FirestoreDataConverter, QueryDocumentSnapshot, WithFieldValue } from "firebase/firestore";
+import { doc } from "firebase/firestore";
+import { collection, getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import type { TPost } from "src/types";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,7 +18,50 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore();
+const firestore = getFirestore();
 const storage = getStorage();
 
-export { app, db, storage };
+const converter = <T extends object>(validator: (data: unknown) => asserts data is T): FirestoreDataConverter<T> => ({
+  toFirestore: (data: WithFieldValue<T>) => data,
+  fromFirestore: (snapshot: QueryDocumentSnapshot) => {
+    const data = snapshot.data();
+
+    const result = Object.fromEntries(
+      Object.entries(data).map(([key, value]) => {
+        if (typeof value.toString == "function" && value.toString().startsWith("Timestamp")) {
+          return [key, value.toDate()];
+        }
+        return [key, value];
+      })
+    );
+
+    validator(result);
+
+    return result;
+  },
+});
+
+const assertPost: (data: unknown) => asserts data is TPost = (data) => {
+  const d = data as Partial<TPost>;
+  if (
+    !(
+      typeof d.username === "string" &&
+      typeof d.profileImg === "string" &&
+      (typeof d.image === "string" || undefined) &&
+      typeof d.caption === "string"
+    )
+  ) {
+    throw new Error("data is not TPost type");
+  }
+};
+
+const dataPoint = <T extends object>(collectionPath: string, validator: (data: unknown) => asserts data is T) =>
+  collection(firestore, collectionPath).withConverter(converter<T>(validator));
+
+const db = {
+  posts: dataPoint<TPost>("posts", assertPost),
+};
+
+const customDoc = (path: string, pathSegments: string) => doc(firestore, path, pathSegments);
+
+export { app, customDoc, db, firestore, storage };
